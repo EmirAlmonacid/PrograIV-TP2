50ca19f67158ab054e86437b9a40704628542bb6
EmirAlmonacid <emiralmonacidutn@gmail.com>
2026-06-02 22:08:48 -0300

    Adaptacion Nest para Vercel

--- red-social-back/src/main.ts
+++ red-social-back/src/main.ts
@@ -1,11 +1,32 @@
 import { NestFactory } from '@nestjs/core';
+import { INestApplication } from '@nestjs/common';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+let app: INestApplication;
 
-  app.enableCors();
-  
-  await app.listen(process.env.PORT ?? 3000);
+async function createApp(): Promise<INestApplication> {
+  if (!app) {
+    app = await NestFactory.create(AppModule);
+
+    app.enableCors({
+      origin: [
+        'http://localhost:4200',
+        'https://progra-iv-tp-2.vercel.app'
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+      allowedHeaders: ['Content-Type'],
+    });
+
+    await app.init();
+  }
+
+  return app;
 }
-bootstrap();
+
+export default async function handler(req: any, res: any) {
+  const app = await createApp();
+
+  const server = app.getHttpAdapter().getInstance();
+
+  return server(req, res);
+}
