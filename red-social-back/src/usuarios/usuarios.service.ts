b46afa7a91b777259d43c695a0f8b4ec2e3ca50f
EmirAlmonacid <emiralmonacidutn@gmail.com>
2026-06-12 19:41:34 -0300

    Debug duplicados

--- red-social-back/src/usuarios/usuarios.service.ts
+++ red-social-back/src/usuarios/usuarios.service.ts
@@ -45,7 +45,26 @@ export class UsuariosService {
     usuario.foto = (imagenSubida as any).secure_url;
 
   }
+const usuarioExistente =
+  await this.usuarioModel.findOne({
+    $or: [
+      { usuario: usuario.usuario },
+      { correo: usuario.correo }
+    ]
+  });
 
+console.log('USUARIO EXISTENTE:', usuarioExistente);
+
+if (usuarioExistente) {
+
+  console.log('ENTRO AL IF');
+
+  return {
+    error: true,
+    mensaje: 'Usuario o correo ya existe'
+  };
+
+}
   const passwordEncriptada =
     await bcrypt.hash(usuario.password, 10);
 
