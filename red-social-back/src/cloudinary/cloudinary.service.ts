3a2ee2e3bc690d220c459523b1d9b5f3e59225c5
EmirAlmonacid <emiralmonacidutn@gmail.com>
2026-06-12 17:31:20 -0300

    remove cloudinary debug

--- red-social-back/src/cloudinary/cloudinary.service.ts
+++ red-social-back/src/cloudinary/cloudinary.service.ts
@@ -1,35 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { v2 as cloudinary } from 'cloudinary';
-import { Express } from 'express';
 
 @Injectable()
 export class CloudinaryService {
-    constructor() {
 
-  throw new Error(`
-  CLOUD_NAME=${process.env.CLOUDINARY_CLOUD_NAME}
-  API_KEY=${process.env.CLOUDINARY_API_KEY}
-  SECRET=${process.env.CLOUDINARY_API_SECRET?.substring(0,10)}
-  `);
+  constructor() {
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+  }
 
-}
-
-    async uploadImage(file: any) {
-        return new Promise((resolve, reject) => {
-            cloudinary.uploader
+  async uploadImage(file: any) {
+    return new Promise((resolve, reject) => {
+      cloudinary.uploader
         .upload_stream(
-        {
+          {
             folder: 'red-social/perfiles',
-            },
-            (error, result) => {
+          },
+          (error, result) => {
             if (error) {
-                reject(error);
+              reject(error);
             } else {
-                resolve(result);
+              resolve(result);
             }
-            },
+          },
         )
         .end(file.buffer);
     });
-    }
+  }
 }
