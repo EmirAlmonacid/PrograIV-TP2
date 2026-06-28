a06c625aa663aac88b26f318d8c0b20eb73d19e9
EmirAlmonacid <emiralmonacidutn@gmail.com>
2026-06-28 17:56:36 -0300

    Estadisticas Front

--- red-social-front/src/app/pages/dashboard-estadisticas/dashboard-estadisticas.ts
+++ red-social-front/src/app/pages/dashboard-estadisticas/dashboard-estadisticas.ts
@@ -3,6 +3,10 @@
     AfterViewInit
     } from '@angular/core';
 
+    import { Chart } from 'chart.js/auto';
+
+    import { PublicacionesService } from '../../../services/publicaciones';
+
     @Component({
     selector: 'app-dashboard-estadisticas',
     imports: [],
@@ -11,9 +15,143 @@
     })
     export class DashboardEstadisticas implements AfterViewInit {
 
-    constructor() {}
+    constructor(
+        private publicacionesService: PublicacionesService
+    ) {}
 
     ngAfterViewInit(): void {
+
+        this.cargarGraficoPublicaciones();
+
+        this.cargarGraficoComentarios();
+
+        this.cargarGraficoComentariosDia();
+
+    }
+
+    cargarGraficoPublicaciones() {
+
+        this.publicacionesService
+        .obtenerEstadisticasPublicaciones()
+        .subscribe({
+
+            next: (datos: any[]) => {
+
+            new Chart('graficoPublicaciones', {
+
+                type: 'bar',
+
+                data: {
+
+                labels: datos.map(
+                    d => d.usuario
+                ),
+
+                datasets: [
+
+                    {
+
+                    label: 'Publicaciones',
+
+                    data: datos.map(
+                        d => d.cantidad
+                    )
+
+                    }
+
+                ]
+
+                }
+
+            });
+
+            }
+
+        });
+
+    }
+
+    cargarGraficoComentarios() {
+
+        this.publicacionesService
+        .obtenerEstadisticasComentarios()
+        .subscribe({
+
+            next: (datos: any[]) => {
+
+            new Chart('graficoComentarios', {
+
+                type: 'bar',
+
+                data: {
+
+                labels: datos.map(
+                    d => d.titulo
+                ),
+
+                datasets: [
+
+                    {
+
+                    label: 'Comentarios',
+
+                    data: datos.map(
+                        d => d.comentarios
+                    )
+
+                    }
+
+                ]
+
+                }
+
+            });
+
+            }
+
+        });
+
+    }
+
+    cargarGraficoComentariosDia() {
+
+        this.publicacionesService
+        .obtenerEstadisticasComentariosPorDia()
+        .subscribe({
+
+            next: (datos: any[]) => {
+
+            new Chart('graficoComentariosDia', {
+
+                type: 'line',
+
+                data: {
+
+                labels: datos.map(
+                    d => d.fecha
+                ),
+
+                datasets: [
+
+                    {
+
+                    label: 'Comentarios por día',
+
+                    data: datos.map(
+                        d => d.cantidad
+                    )
+
+                    }
+
+                ]
+
+                }
+
+            });
+
+            }
+
+        });
 
     }
 
